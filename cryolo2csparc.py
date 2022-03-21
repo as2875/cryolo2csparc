a255bc59e61b7fad15ffa3132471ebdd0be88af3
Alexander Shtyrov <67324165+as2875@users.noreply.github.com>
2022-03-21 16:09:54 +0000

    Initial commit

---
+++ cryolo2csparc.py
@@ -0,0 +1,42 @@
+import argparse
+import glob
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--cryolo", metavar="PATTERN",
+                    help="STAR files written by crYOLO")
+parser.add_argument("--output",
+                    help="Location of output STAR file")
+parser.add_argument("--doseweighted", action="store_true", default=False,
+                    help="Whether to append '_doseweighted' to filenames")
+args = parser.parse_args()
+
+#CRYOLO_GLOB = "/home/cryosparcuser/Shikang/20220303/cryolo_particles_thresh0d1/STAR/*.star"
+#OUTPUT_FILE = "/home/cryosparcuser/Shikang/20220303/cryolo_particles_thresh0d1.star"
+
+complete_file = ["\n",
+                 "data_\n",
+                 "\n",
+                 "loop_\n",
+                 "_rlnMicrographName #1\n",
+                 "_rlnCoordinateX #2\n",
+                 "_rlnCoordinateY #3\n"]
+
+for star_file in glob.glob(args.cryolo):
+    with open(star_file) as f:
+        star_contents = f.readlines()
+    for line in star_contents:
+        if line.startswith("_"):
+            continue
+        fields = line.split()
+        if len(fields) == 2:
+            if args.doseweighted:
+                filename = os.path.splitext(os.path.split(star_file)[1])[0] + "_doseweighted.mrc"
+            else:
+                filename = os.path.splitext(os.path.split(star_file)[1])[0] + ".mrc"
+
+            new_line = filename + "\t" + line
+            complete_file = complete_file + [new_line]
+
+with open(args.output, "w") as f:
+    f.writelines(complete_file)
